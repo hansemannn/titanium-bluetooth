4c865d13b2fb120e586c8e5bf3032bf9a33603fb
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-12-20 10:06:20 +0100

    Expose all peripheral methods

--- iphone/Classes/TiBluetoothPeripheralProxy.m
+++ iphone/Classes/TiBluetoothPeripheralProxy.m
@@ -7,6 +7,9 @@
 
 #import "TiBluetoothPeripheralProxy.h"
 #import "TiBluetoothServiceProxy.h"
+#import "TiBluetoothCharacteristicProxy.h"
+#import "TiBluetoothDescriptorProxy.h"
+#import "TiBluetoothUtils.h"
 #import "TiUtils.h"
 
 @implementation TiBluetoothPeripheralProxy
@@ -54,61 +57,107 @@
 
 - (void)discoverServices:(id)args
 {
-    NSMutableArray<CBUUID*> *uuids = [NSMutableArray array];
-    
-    for (id uuid in args) {
-        ENSURE_TYPE(uuid, NSString);
-        [uuids addObject:[CBUUID UUIDWithString:[TiUtils stringValue:uuid]]];
-    }
-
-    [peripheral discoverServices:uuids];
+    [peripheral discoverServices:[TiBluetoothUtils UUIDArrayFromStringArray:args]];
 }
-
-// TODO: Implement other CBPeripheral methods
 
 - (void)discoverIncludedServicesForService:(id)args
 {
+    ENSURE_ARG_COUNT(args, 2);
     
+    id includedServices = [args objectAtIndex:0];
+    id service = [args objectAtIndex:1];
+    
+    ENSURE_TYPE(includedServices, NSArray);
+    ENSURE_TYPE(service, TiBluetoothServiceProxy);
+    
+    [peripheral discoverIncludedServices:[TiBluetoothUtils UUIDArrayFromStringArray:includedServices]
+                              forService:[(TiBluetoothServiceProxy *)service service]];
 }
 
 - (void)discoverCharacteristicsForService:(id)args
 {
+    ENSURE_ARG_COUNT(args, 2);
     
+    id characteristics = [args objectAtIndex:0];
+    id service = [args objectAtIndex:1];
+    
+    ENSURE_TYPE(characteristics, NSArray);
+    ENSURE_TYPE(service, TiBluetoothServiceProxy);
+
+    [peripheral discoverCharacteristics:[TiBluetoothUtils UUIDArrayFromStringArray:characteristics]
+                             forService:[(TiBluetoothServiceProxy *)service service]];
 }
 
 - (void)readValueForCharacteristic:(id)value
 {
+    ENSURE_SINGLE_ARG(value, TiBluetoothCharacteristicProxy);
     
+    [peripheral readValueForCharacteristic:[(TiBluetoothCharacteristicProxy*)value characteristic]];
 }
 
 - (id)maximumWriteValueLengthForType:(id)value
 {
-    
+    return NUMUINTEGER([peripheral maximumWriteValueLengthForType:[TiUtils intValue:value]]);
 }
 
 - (void)writeValueForCharacteristicWithType:(id)args
 {
+    ENSURE_ARG_COUNT(args, 3);
     
+    id value = [args objectAtIndex:0];
+    id characteristic = [args objectAtIndex:1];
+    id type = [args objectAtIndex:2];
+    
+    ENSURE_TYPE(value, TiBlob);
+    ENSURE_TYPE(characteristic, TiBluetoothCharacteristicProxy);
+    ENSURE_TYPE(type, NSNumber);
+
+    
+    [peripheral writeValue:[(TiBlob*)value data]
+         forCharacteristic:[(TiBluetoothCharacteristicProxy*)characteristic characteristic]
+                      type:[TiUtils intValue:type]];
 }
 
 - (void)setNotifyValueForCharacteristic:(id)args
 {
+    ENSURE_ARG_COUNT(args, 2);
     
+    id notifyValue = [args objectAtIndex:0];
+    id characteristic = [args objectAtIndex:1];
+    
+    ENSURE_TYPE(notifyValue, NSNumber);
+    ENSURE_TYPE(characteristic, TiBluetoothCharacteristicProxy);
+    
+    [peripheral setNotifyValue:[TiUtils boolValue:notifyValue]
+             forCharacteristic:[(TiBluetoothCharacteristicProxy*)characteristic characteristic]];
 }
 
 - (void)discoverDescriptorsForCharacteristic:(id)value
 {
+    ENSURE_SINGLE_ARG(value, TiBluetoothCharacteristicProxy);
     
+    [peripheral discoverDescriptorsForCharacteristic:[(TiBluetoothCharacteristicProxy*)value characteristic]];
 }
 
 - (void)readValueForDescriptor:(id)value
 {
-    
+    ENSURE_SINGLE_ARG(value, TiBluetoothDescriptorProxy);
+
+    [peripheral readValueForDescriptor:[(TiBluetoothDescriptorProxy*)value descriptor]];
 }
 
 - (void)writeValueForDescriptor:(id)args
 {
+    ENSURE_ARG_COUNT(args, 2);
     
+    id value = [args objectAtIndex:0];
+    id descriptor = [args objectAtIndex:1];
+    
+    ENSURE_TYPE(value, TiBlob);
+    ENSURE_TYPE(descriptor, TiBluetoothDescriptorProxy);
+    
+    [peripheral writeValue:[(TiBlob*)value data]
+             forDescriptor:[(TiBluetoothDescriptorProxy*)descriptor descriptor]];
 }
 
 #pragma mark Utilities
