7ff573eb3b1f6992c9a7153735e932dd7ddda6b3
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-12-22 18:52:39 +0100

    Migrate readme and folders

---
+++ ios/Classes/TiBluetoothModuleAssets.m
@@ -0,0 +1,24 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "TiBluetoothModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation TiBluetoothModuleAssets
+
+- (NSData*) moduleAsset
+{
+	
+
+	return nil;
+}
+
+- (NSData*) resolveModuleAsset:(NSString*)path
+{
+	
+
+	return nil;
+}
+
+@end
