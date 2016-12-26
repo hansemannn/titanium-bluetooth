8c7774c9b58e72071228eb7bf717147f19c9a96c
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-12-26 13:38:31 +0100

    Update .gitignore

--- ios/build/ti.bluetooth.build/Release-iphoneos/ti.bluetooth.build/Objects-normal/arm64/TiBluetoothModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hknoechel/Documents/appcelerator_modules/ti.bluetooth/iphone/Classes/TiBluetoothModuleAssets.m \
-  /Users/hknoechel/Documents/appcelerator_modules/ti.bluetooth/iphone/Classes/TiBluetoothModuleAssets.h
