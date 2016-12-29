1fdc123347c8c5f4e4dacdbda38cb4f633e5fd6d
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-12-29 08:26:19 +0100

    Add missing CBATTRequest properties, fix blobs

--- ios/Classes/TiBluetoothRequestProxy.m
+++ ios/Classes/TiBluetoothRequestProxy.m
@@ -6,6 +6,9 @@
  */
 
 #import "TiBluetoothRequestProxy.h"
+#import "TiBluetoothCentralProxy.h"
+#import "TiBluetoothCharacteristicProxy.h"
+#import "TiBlob.h"
 
 @implementation TiBluetoothRequestProxy
 
@@ -21,6 +24,28 @@
 - (CBATTRequest *)request
 {
     return request;
+}
+
+#pragma mark Public APIs
+
+- (TiBluetoothCentralProxy *)central
+{
+    return [[TiBluetoothCentralProxy alloc] _initWithPageContext:[self pageContext] andCentral:request.central];
+}
+
+- (TiBluetoothCharacteristicProxy *)characteristic
+{
+    return [[TiBluetoothCharacteristicProxy alloc] _initWithPageContext:[self pageContext] andCharacteristic:request.characteristic];
+}
+
+- (NSNumber *)offset
+{
+    return NUMUINTEGER(request.offset);
+}
+
+- (TiBlob *)value
+{
+    return [[TiBlob alloc] _initWithPageContext:[self pageContext] andData:request.value mimetype:@"text/plain"];
 }
 
 @end
